06d4765d3240b575bdc828c82a2527b58dbca565
Atharv Vaish <avaish@caltech.edu>
2013-03-29 22:50:37 -0400

    beginning api

--- hackprinceton/rdtrpr/views.py
+++ hackprinceton/rdtrpr/views.py
@@ -7,3 +7,12 @@ def welcome(request):
         'current_time': "3PM",
     })
 	return HttpResponse(t.render(c))
+    
+def add_loc(request):
+	if request.method == 'POST':
+		try:
+			print request.POST['url']
+			return HttpResponse(request.POST['url'], mimetype="application/json")
+		except KeyError:
+			print 'Where is my url?'
+			return HttpResponse("error", mimetype="application/json")
