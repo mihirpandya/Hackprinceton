001e119c097e0816ec60a1d389ca76c8a4c9fccb
Mihir Pandya <mihir.m.pandya@gmail.com>
2013-03-29 20:00:29 -0400

    Added rdtrpr

--- hackprinceton/hackprinceton/settings.py
+++ hackprinceton/hackprinceton/settings.py
@@ -155,6 +155,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rdtrpr'
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
