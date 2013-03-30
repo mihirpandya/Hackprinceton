31dca3859ea2e7096e6d29047b2e71a9e5843591
Mihir Pandya <mihir.m.pandya@gmail.com>
2013-03-29 20:02:28 -0400

    Working rdtrpr

--- hackprinceton/rdtrpr/models.py
+++ hackprinceton/rdtrpr/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class Location(models.Model):
 	l_id = 	models.AutoField(primary_key=True)
-    longitude = models.DecimalField(max_digits=10, decimal_places=7)
-    latitude = models.DecimalField(max_digits=10, decimal_places=7)
-    url = models.TextField()
-    time = models.DateField()
+	longitude = models.DecimalField(max_digits=10, decimal_places=7)
+	latitude = models.DecimalField(max_digits=10, decimal_places=7)
+	url = models.TextField()
+	time = models.DateField()
 
 class Wikipedia(models.Model):
 	w_id = models.AutoField(primary_key=True)
