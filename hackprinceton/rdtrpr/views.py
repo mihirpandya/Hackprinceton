e056e304c1555982ec44480d50c10e8fb8a44e65
Mihir Pandya <mihir.m.pandya@gmail.com>
2013-03-30 00:46:24 -0400

    fixed conflict

--- hackprinceton/rdtrpr/views.py
+++ hackprinceton/rdtrpr/views.py
@@ -25,17 +25,14 @@ def add_loc(request):
 	if request.method == 'POST':
 		response_data = {}
 		try:
-			loc = Location.objects.create_loc(request.POST['lat'], \
-				request.POST['long'], request.POST['url'])
+			loc = Location.objects.create_loc(request.POST['lat'],request.POST['long'], request.POST['url'])
 			loc.save()
 			response_data['result'] = 'success'
 			response_data['message'] = 'You did not mess up'
-			return HttpResponse(json.dumps(response_data), \
-				content_type="application/json")
+			return HttpResponse(json.dumps(response_data),content_type="application/json")
 		except KeyError:
 			print 'Where is my url?'
 			return HttpResponse("error", mimetype="application/json")
 			response_data['result'] = 'failed'
 			response_data['message'] = 'You messed up'
-			return HttpResponse(json.dumps(response_data), \
-				content_type="application/json")
+			return HttpResponse(json.dumps(response_data),content_type="application/json")
